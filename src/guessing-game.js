8a24cc20d5a73886abd282afb1f942b61902b553
Anastasiia Pyzhevska <pyzhiknastya2912@gmail.com>
2023-02-23 16:26:22 +0200

    task guessing-game done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        if (this.min + 1 == this.max - 1) {
+            return this.min + 1;
+        } else {
+            return Math.ceil((this.max + this.min) / 2)
+        }
     }
 
     lower() {
-
+        return this.max = this.guess();
     }
 
     greater() {
-
+        return this.min = this.guess();
     }
 }
 
